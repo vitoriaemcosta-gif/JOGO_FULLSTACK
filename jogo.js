a075eafc8ef17d1baece405d29197c95af54edd2
Vitoria Emanuelly Machado Costa <vitoriae.m.costa@gmail.com>
2025-11-10 21:37:35 -0300

    jogo

--- jogo.js
+++ jogo.js
@@ -5,16 +5,16 @@ function imagem(url, x, y) {
   var img = new Image();
   img.src = url;
   img.onload = function () {
-    ctx.drawImage(img, x, y, 240, 280);
+    ctx.drawImage(img, x, y, 200, 240);
   };
   return img;
 }
 
 // Chamando a função para cada imagem com sua posição
-var caixa_vermelho = imagem('imagens/4.png', 20, 170);
-var caixa_azul = imagem('imagens/1.png', 165, 114);
-var caixa_amarelo = imagem('imagens/3.png', 310, 30);
-var caixa_verde = imagem('imagens/2.png', 455, -15);
+var caixa_vermelho = imagem('imagens/4.png', 50, 250);
+var caixa_azul = imagem('imagens/1.png', 155, 222);
+var caixa_amarelo = imagem('imagens/3.png', 270, 161);
+var caixa_verde = imagem('imagens/2.png', 385, 130);
 
 
 
@@ -23,42 +23,13 @@ function imagem2(url, x, y) {
   var img2 = new Image();
   img2.src = url;
   img2.onload = function () {
-    ctx.drawImage(img2, x, y, 800, 710);
+    ctx.drawImage(img2, x, y, 590, 500);
   };
   return img2;
 }
 
 // Chamando a função para cada imagem com sua posição
-var esteira = imagem2('imagens/esteira.png', 40, 82);
+var esteira = imagem2('imagens/esteira1.png', 190, 212);
 
 
- // Lista dos frames (substitua pelos caminhos corretos das imagens)
- const frames = [
-  "imagens/lixeirafogo_frame_dif_1.png",
-  // "imagens/lixeirafogo_frame_dif_5.png",
-  "imagens/lixeirafogo_frame_dif_10.png"
-];
 
-const images = [];
-let currentFrame = 0;
-
-// Carregar as imagens
-frames.forEach((src, i) => {
-  const img = new Image();
-  img.src = src;
-  images.push(img);
-});
-
-function animate() {
-  const img = images[currentFrame];
-  if (img.complete) {
-    ctx.drawImage(img, 650, 170, 170, 220);
-  }
-  currentFrame = (currentFrame + 1) % images.length;
-  setTimeout(animate, 300); // muda a velocidade da animação
-}
-
-// Inicia a animação quando as imagens estiverem carregadas
-window.onload = () => {
-  animate();
-};
